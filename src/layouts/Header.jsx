aeb6ebda358894c8b43108e03f9786ea490cb486
Aidos Kanapyanov <aidos.kanapyanov@alumni.nu.edu.kz>
2023-02-24 00:48:56 +0600

    add mobile header

---
+++ src/layouts/Header.jsx
@@ -0,0 +1,14 @@
+function Header() {
+  return (
+    <>
+      <header className="bg-white flex items-center px-4 md:py-4 py-2 shadow">
+        <h1 className="text-sm grow uppercase tracking-widest font-bold">
+          Aidos Kanapyanov
+        </h1>
+        <div>☰</div>
+      </header>
+    </>
+  );
+}
+
+export default Header;
