92689071ae96735f42d4aa04693a6de5c85c18cd
Aidos Kanapyanov <aidos.kanapyanov@alumni.nu.edu.kz>
2023-02-24 03:43:26 +0600

    ref: rename header link

--- src/layouts/header/HeaderLinks.jsx
+++ src/layouts/header/HeaderLinks.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export default function HeaderLinks() {
   const navLinks = [
     {
-      name: "Home",
+      name: "Site",
       href: "/site",
     },
     {
