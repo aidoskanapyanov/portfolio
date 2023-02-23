5e2fa91cd6acf72d75ce5ab6c10a9ffb44796518
Aidos Kanapyanov <aidos.kanapyanov@alumni.nu.edu.kz>
2023-02-24 03:25:02 +0600

    feat: add about site page

---
+++ src/pages/AboutSite.jsx
@@ -0,0 +1,19 @@
+import Page from "../components/page";
+
+export default function About() {
+  return (
+    <>
+      <Page
+        title="About this site"
+        subtitle="a beautiful, responsive, statically-generated, react application written with modern javascript."
+      >
+        <p className="mb-4">
+          Welcome to my website. Please feel free to read more about me, or you
+          can check out my resume, projects, view site statistics, or contact
+          me.
+        </p>
+        <p className="mb-4">Source available here.</p>
+      </Page>
+    </>
+  );
+}
