b1f0038926beeb06e5269d7ed34d5246dabd6967
aidoskanapyanov <aidos.kanapyanov@alumni.nu.edu.kz>
2023-02-24 12:34:23 +0600

    feat!: scroll to top on route change

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter } from "react-router-dom";
+import ScrollToTop from "./components/ScrollToTop";
 import Router from "./routes";
 
 export default function App() {
   return (
     <>
       <BrowserRouter>
+        <ScrollToTop />
         <Router />
       </BrowserRouter>
     </>
