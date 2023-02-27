5532d85634a3d35673f79863dd6ef1a7f35ca778
Aidos Kanapyanov <aidos.kanapyanov@alumni.nu.edu.kz>
2023-02-28 01:31:00 +0600

    fix: rm unused z index

--- src/layouts/base/navbar/Navbar.jsx
+++ src/layouts/base/navbar/Navbar.jsx
@@ -27,7 +27,7 @@ export default function Navbar({ openNav, onCloseNav }) {
         ref={ref}
         className={`${
           openNav ? "" : "translate-x-full"
-        } transition ease-in duration-200 shadow dark:border-l-gray-500 dark:border-l fixed right-0 top-0 h-screen z-[999] bg-white dark:bg-slate-800 dark:text-white pl-8 pr-4 py-2`}
+        } transition ease-in duration-200 shadow dark:border-l-gray-500 dark:border-l fixed right-0 top-0 h-screen bg-white dark:bg-slate-800 dark:text-white pl-8 pr-4 py-2`}
       >
         <div className="flex justify-end">
           <button className="block right-0" onClick={onCloseNav}>
