ca65c9e039b1a212b93d8f263aaf6c9aa3a28871
aidoskanapyanov <aidos.kanapyanov@alumni.nu.edu.kz>
2023-02-27 15:37:13 +0600

    feat!: implement 404

---
+++ src/pages/Page404.jsx
@@ -0,0 +1,14 @@
+// tailwind css page 404
+export default function Page404() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <h1 className="text-6xl font-bold">404</h1>
+      <p className="text-2xl font-medium">Page not found.</p>
+      <p className="mt-4">
+        <a href="/" className="text-blue-600 hover:underline">
+          Go back home
+        </a>
+      </p>
+    </div>
+  );
+}
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,7 +1,9 @@
 import { Navigate, useRoutes } from "react-router-dom";
 import BaseLayout from "../layouts/base/";
+import SimpleLayout from "../layouts/simple/";
 import About from "../pages/About";
 import AboutSite from "../pages/AboutSite";
+import Page404 from "../pages/Page404";
 
 export default function Router() {
   const routes = useRoutes([
@@ -13,6 +15,19 @@ export default function Router() {
         { path: "/about", element: <About /> },
         { path: "/site", element: <AboutSite /> },
       ],
+    },
+    {
+      element: <SimpleLayout />,
+      children: [
+        { element: <Navigate to="/dashboard/app" />, index: true },
+        { path: "404", element: <Page404 /> },
+        { path: "*", element: <Navigate to="/404" />, status: 404 },
+      ],
+    },
+    {
+      path: "*",
+      element: <Navigate to="/404" replace />,
+      status: 404,
     },
   ]);
 
