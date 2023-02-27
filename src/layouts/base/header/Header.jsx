5ffda4c8a7b5988815fe2dc60edde9fd3e0fcefb
Aidos Kanapyanov <aidos.kanapyanov@alumni.nu.edu.kz>
2023-02-28 02:18:14 +0600

    feat!: useHideHeader hook

--- src/layouts/base/header/Header.jsx
+++ src/layouts/base/header/Header.jsx
@@ -1,11 +1,18 @@
 import { Link } from "react-router-dom";
 import HeaderLinks from "./HeaderLinks";
 import Switcher from "../../../components/Switcher";
+import useHideHeader from "../../../hooks/useHideHeader";
 
 function Header({ onOpenNav }) {
+  const [visible] = useHideHeader();
+
   return (
     <>
-      <header className="bg-white dark:bg-slate-800 dark:text-white flex gap-x-4 md:divide-x  items-center px-4 md:py-4 py-2 shadow">
+      <header
+        className={`sticky top-0 ${
+          visible ? "" : "-translate-y-full"
+        } transition-transform ease-in duration-200 bg-white dark:bg-slate-800 dark:text-white flex gap-x-4 md:divide-x  items-center p-4 shadow`}
+      >
         <h1 className="transition ease-in duration-200 hover:text-blue-900 dark:hover:text-blue-300 text-sm xs:grow uppercase tracking-widest font-bold">
           <Link to="/about" className="text-sm uppercase tracking-widest">
             Aidos Kanapyanov
