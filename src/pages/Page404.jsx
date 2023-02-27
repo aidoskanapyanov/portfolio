3e638b00a67fa3f8d093f535184c1634a9ca8c97
aidoskanapyanov <aidos.kanapyanov@alumni.nu.edu.kz>
2023-02-27 18:21:42 +0600

    feat!: 404 svg

--- src/pages/Page404.jsx
+++ src/pages/Page404.jsx
@@ -1,5 +1,6 @@
 import { Helmet } from "react-helmet-async";
 import { Link } from "react-router-dom";
+import img404 from "../assets/images/404.svg";
 
 export default function Page404() {
   return (
@@ -7,13 +8,14 @@ export default function Page404() {
       <Helmet>
         <title>404 | Aidos Kanapyanov</title>
       </Helmet>
-      <div className="dark:text-white flex flex-col items-center justify-center min-h-screen">
+      <div className="dark:text-white flex flex-col items-center justify-center min-h-screen pb-52">
+        <img src={img404} alt="" className="max-h-80" />
         <h1 className="text-6xl font-bold">404</h1>
         <p className="text-2xl font-medium">Page not found.</p>
-        <p className="mt-4">
+        <p className="mt-8">
           <Link
             to="/"
-            className="transition ease-in duration-200 text-blue-600 hover:underline"
+            className="transition ease-in duration-200 p-4 text-xs font-medium uppercase border tracking-widest hover:text-blue-900 dark:hover:text-blue-300 hover:border-blue-900 dark:hover:border-blue-300"
           >
             Go back home
           </Link>
