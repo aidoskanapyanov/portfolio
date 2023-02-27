ac7ad92f9f491286de3002d582532a2066f98e9c
aidoskanapyanov <aidos.kanapyanov@alumni.nu.edu.kz>
2023-02-27 17:19:07 +0600

    fix: 404 link to home page

--- src/pages/Page404.jsx
+++ src/pages/Page404.jsx
@@ -1,16 +1,17 @@
-// tailwind css page 404
+import { Link } from "react-router-dom";
+
 export default function Page404() {
   return (
     <div className="dark:text-white flex flex-col items-center justify-center min-h-screen">
       <h1 className="text-6xl font-bold">404</h1>
       <p className="text-2xl font-medium">Page not found.</p>
       <p className="mt-4">
-        <a
-          href="/"
+        <Link
+          to="/"
           className="transition ease-in duration-200 text-blue-600 hover:underline"
         >
           Go back home
-        </a>
+        </Link>
       </p>
     </div>
   );
