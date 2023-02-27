f1466eac308a9f52086fd6445659a96ba05209d7
aidoskanapyanov <aidos.kanapyanov@alumni.nu.edu.kz>
2023-02-27 17:54:20 +0600

    feat: wrap with helmet provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import { BrowserRouter } from "react-router-dom";
 import ScrollToTop from "./components/ScrollToTop";
 import Router from "./routes";
+import { HelmetProvider } from "react-helmet-async";
 
 export default function App() {
   return (
     <>
-      <BrowserRouter>
-        <ScrollToTop />
-        <Router />
-      </BrowserRouter>
+      <HelmetProvider>
+        <BrowserRouter>
+          <ScrollToTop />
+          <Router />
+        </BrowserRouter>
+      </HelmetProvider>
     </>
   );
 }
