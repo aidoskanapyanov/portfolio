4d26ea9f97dfe2f99b08995a78044097962862e4
aidoskanapyanov <aidos.kanapyanov@alumni.nu.edu.kz>
2023-02-28 15:59:03 +0600

    rm: hide navbar

--- src/layouts/base/header/Header.jsx
+++ src/layouts/base/header/Header.jsx
@@ -1,18 +1,11 @@
 import { Link } from "react-router-dom";
 import HeaderLinks from "./HeaderLinks";
 import Switcher from "../../../components/Switcher";
-import useHideHeader from "../../../hooks/useHideHeader";
 
 function Header({ onOpenNav }) {
-  const visible = useHideHeader();
-
   return (
     <>
-      <header
-        className={`sticky top-0 ${
-          visible ? "" : "-translate-y-full"
-        } transition-transform ease-in duration-200 bg-white dark:bg-slate-800 dark:text-white flex gap-x-4 md:divide-x  items-center p-4 shadow dark:shadow-none dark:border-b-[1.5px] dark:border-b-gray-500`}
-      >
+      <header className="transition-transform ease-in duration-200 sticky top-0 bg-white dark:bg-slate-800 dark:text-white flex gap-x-4 md:divide-x  items-center p-4 shadow dark:shadow-none dark:border-b-[1.5px] dark:border-b-gray-500">
         <h1 className="transition ease-in duration-200 hover:text-blue-900 dark:hover:text-blue-300 text-sm xs:grow uppercase tracking-widest font-bold">
           <Link to="/about" className="text-sm uppercase tracking-widest">
             Aidos Kanapyanov
